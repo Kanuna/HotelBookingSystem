cb86fc2487a86a70f50aa181256bb41d4f242cf4
Boje <math039rrelev.techcollege.dk>
2025-10-27 14:43:15 +0100

    Removed unnecessary imports

--- src/main/java/com/boje/hotelbooking/repositories/AddressRepository.java
+++ src/main/java/com/boje/hotelbooking/repositories/AddressRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.boje.hotelbooking.models.Address;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
